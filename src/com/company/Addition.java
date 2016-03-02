62724f9d45fc9e47d40128596de59133eb7b655c
bustercg <brianhorne72@yahoo.com>
2016-03-01 23:35:22 -0500

    Finished addition and getInput and got the main set up

--- src/com/company/Addition.java
+++ src/com/company/Addition.java
@@ -4,4 +4,7 @@ package com.company;
  * Created by battl_4m39d0w on 3/1/2016.
  */
 public class Addition {
+    public static void add(){
+        Main.answer = Main.x + Main.y;
+    }
 }
--- src/com/company/GetInput.java
+++ src/com/company/GetInput.java
@@ -1,7 +1,16 @@
 package com.company;
+import java.util.Scanner;
 
 /**
  * Created by battl_4m39d0w on 3/1/2016.
  */
 public class GetInput {
+    public static void getNumbers()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter yo number bitch");
+        Main.x = keyboard.nextInt();
+        Main.sign = keyboard.next();
+        Main.y = keyboard.nextInt();
+    }
 }
